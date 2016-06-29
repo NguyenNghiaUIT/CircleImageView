aba03c792b53069d51191ea50d15e5132a82e4f8
NguyenNghiaUIT <nghiaklpro195@gmail.com>
2016-06-29 08:56:10 +0700

    add activity test case circle containt 1 bitmap and 1 text

---
+++ app/src/main/java/com/example/nguyennghia/circleimageview/TwoCircleImageActivity.java
@@ -0,0 +1,22 @@
+package com.example.nguyennghia.circleimageview;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TwoCircleImageActivity extends AppCompatActivity {
+
+    CircleImage circleImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_circle_image);
+
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), +R.drawable.ava1);
+        circleImage = (CircleImage) findViewById(R.id.ci_two_image);
+        circleImage.setBitmapUrl("url", "1");
+        circleImage.drawBitmapAt(bm, 0, true);
+    }
+}
